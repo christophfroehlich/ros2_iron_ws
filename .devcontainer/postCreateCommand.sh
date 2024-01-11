e6f220e9552d9c0cfcf5cafa6a0219a7a17b8b3e
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-01-11 22:32:47 +0100

    first commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+source /opt/ros/iron/setup.bash
+sudo apt-get update
+rosdep update --rosdistro $ROS_DISTRO
+source /opt/ros/iron/setup.bash
+set -xeu
+vcs import src < ros_control.repos
+# install stuff for control.ros.org
+(cd src/control.ros.org/ && python3 -m pip install -r requirements.txt)
+# Install generate_parameter_library as python package
+# python3 -m pip install pyyaml
+# pip3 install typeguard==4.0.0
+(cd src/generate_parameter_library/generate_parameter_library_py/ && python3 -m pip install .)
+# install other dependencies
+rosdep install -riy --from-paths src
+# if the repo is cloned from windows
+# git config --global --add safe.directory /workspaces/ros2_iron_ws
+# install pre-commit hooks
+(cd src/ros2_controllers && pre-commit install)
+(cd src/ros2_control && pre-commit install)
+(cd src/ros2_control_demos && pre-commit install)
+(cd src/control_toolbox && pre-commit install)
+(cd src/control_msgs && pre-commit install)
+(cd src/control.ros.org && pre-commit install)
+touch src/control.ros.org/COLCON_IGNORE
+echo "*.pyc" > ~/.gitignore
+echo "*__pycache__*" >> ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
+# defaults for colcon mixins
+colcon mixin add default \
+  https://raw.githubusercontent.com/colcon/colcon-mixin-repository/master/index.yaml && \
+  colcon mixin update && \
+  colcon metadata add default \
+  https://raw.githubusercontent.com/colcon/colcon-metadata-repository/master/index.yaml && \
+  colcon metadata update
+mkdir -p ~/.colcon && cp .devcontainer/defaults.yaml ~/.colcon/defaults.yaml
+# alias to prune fork and origin
+echo "alias gitprune='git fetch --prune fork && git fetch --prune origin && git removed-branches --prune --force -r fork && git removed-branches --prune --force -r fork'" >> ~/.bashrc
---
+++ .devcontainer/updateContentCommand.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# echo "export DISPLAY=\"\$(host host.docker.internal | awk '/has address/ { print \$4 ; exit }'):0.0\"" >> "/home/vscode/.bashrc"
+#echo "export AMENT_CPPCHECK_ALLOW_SLOW_VERSIONS=true" >> "/home/vscode/.bashrc"
